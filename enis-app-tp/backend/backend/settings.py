5494ded162f21098a0c20ad41ab98b11ae9b3c98
amine-2001 <123871797+amine-2001@users.noreply.github.com>
2024-12-10 09:06:10 +0100

    Update settings.py

--- enis-app-tp/backend/backend/settings.py
+++ enis-app-tp/backend/backend/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'NAME': 'enis_tp',
         'USER': 'dbuser',
         'PASSWORD': 'DBpassword2024',
-        'HOST': 'mydb.c9kw6kciyldg.us-east-1.rds.amazonaws.com',
+        'HOST': 'DB_HOST',
         'PORT': 3306,
     }
 }
